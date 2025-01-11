2c01876317810b8216519c0ae444a7481cc84768
Reetusinha <145335618+Reetusinha@users.noreply.github.com>
2025-01-11 11:52:27 +0000

    11-01-2025

--- 1_basic.js
+++ 1_basic.js
@@ -1,1 +1,38 @@
+let month = 3
+switch (month) {
+    case 1:
+        console.log("january")
+        
+        break;
+    case 2:
+        console.log("feb")
+        
+        break;
+        
+        case 3:
+            console.log(".march")
+            
+            break;
+        case 4:
+            console.log("april")
+            
+            break;
+    default:
+        console.log("default name")
+        break;
+}
+ 
 
+// let name = "reetu"
+//if (name) {
+  //  console.log(`your name is ${name}`)
+//}else {
+//    console.log("error")
+//  }
+
+let array = [2,3,4,6]
+if (array.length==0) {
+    console.log(array)
+} else {
+    console.log("error")
+}
